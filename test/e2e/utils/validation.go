1977eae72f86c6e6f3d65e5df3cbc41fe4b78ea8
mrhello369 <admin@kubefin.dev>
2023-11-01 19:52:41 +0800

    e2e: add e2e for costs/metrics summary api
    Signed-off-by: mrhello369 <admin@kubefin.dev>

--- test/e2e/utils/validation.go
+++ test/e2e/utils/validation.go
@@ -21,98 +21,55 @@ import (
 )
 
 func ValidateAllClustersMetricsSummary(clustersSummary *api.ClusterMetricsSummaryList) bool {
-	if len(clustersSummary.Items) != 0 &&
-		clustersSummary.Items[0].ClusterConnectionSate == "running" &&
-		clustersSummary.Items[0].NodeNumbersCurrent != 0 &&
-		clustersSummary.Items[0].PodTotalCurrent != 0 &&
-		clustersSummary.Items[0].PodScheduledCurrent != 0 &&
-		clustersSummary.Items[0].CPUCoreTotal != 0 &&
-		clustersSummary.Items[0].RAMGiBTotal != 0 {
-		return true
+	for _, cluster := range clustersSummary.Items {
+		if cluster.ClusterConnectionSate != "running" ||
+			cluster.NodeNumbersCurrent == 0 ||
+			cluster.PodTotalCurrent == 0 ||
+			cluster.PodScheduledCurrent == 0 ||
+			cluster.CPUCoreTotal == 0 ||
+			cluster.RAMGiBTotal == 0 {
+			return false
+		}
 	}
 
-	return false
+	return true
 }
 
 func ValidateSpecificClusterMetricsSummary(clusterSummary *api.ClusterMetricsSummary) bool {
-	if clusterSummary.ClusterConnectionSate == "running" &&
-		clusterSummary.NodeNumbersCurrent != 0 &&
-		clusterSummary.PodTotalCurrent != 0 &&
-		clusterSummary.PodScheduledCurrent != 0 &&
-		clusterSummary.CPUCoreTotal != 0 &&
-		clusterSummary.RAMGiBTotal != 0 {
-		return true
+	if clusterSummary.ClusterConnectionSate != "running" ||
+		clusterSummary.NodeNumbersCurrent == 0 ||
+		clusterSummary.PodTotalCurrent == 0 ||
+		clusterSummary.PodScheduledCurrent == 0 ||
+		clusterSummary.CPUCoreTotal == 0 ||
+		clusterSummary.RAMGiBTotal == 0 {
+		return false
 	}
 
-	return false
+	return true
 }
 
-func ValidateSpecificClusterResourceMetrics(metrics *api.ClusterResourceMetrics) bool {
-	if metrics.ClusterId != "" &&
-		len(metrics.ResourceTotalValues) != 0 &&
-		len(metrics.ResourceUsageValues) != 0 &&
-		len(metrics.ResourceRequestValues) != 0 &&
-		len(metrics.ResourceSystemTakenValues) != 0 {
-		return true
-	}
-	return false
-}
-
-func ValidateAllClustersCostsSummary(clustersCostsSummary *api.ClusterCostsSummaryList) bool {
-	if len(clustersCostsSummary.Items) != 0 &&
-		clustersCostsSummary.Items[0].ClusterConnectionSate == "running" &&
-		clustersCostsSummary.Items[0].ClusterMonthCostCurrent != 0 &&
-		clustersCostsSummary.Items[0].ClusterMonthEstimateCost != 0 &&
-		clustersCostsSummary.Items[0].ClusterAvgDailyCost != 0 &&
-		clustersCostsSummary.Items[0].ClusterAvgHourlyCoreCost != 0 {
-		return true
+func ValidateAllClustersCostsSummary(clustersSummary *api.ClusterCostsSummaryList) bool {
+	for _, cluster := range clustersSummary.Items {
+		if cluster.ClusterConnectionSate != "running" ||
+			cluster.ClusterMonthCostCurrent == 0 ||
+			cluster.ClusterMonthEstimateCost == 0 ||
+			cluster.ClusterAvgDailyCost == 0 ||
+			cluster.ClusterAvgHourlyCoreCost == 0 {
+			return false
+		}
 	}
 
-	return false
+	return true
 }
 
-func ValidateSpecificClusterCostsSummary(clusterCostsSummary *api.ClusterCostsSummary) bool {
-	if clusterCostsSummary.ClusterConnectionSate == "running" &&
-		clusterCostsSummary.ClusterMonthCostCurrent != 0 &&
-		clusterCostsSummary.ClusterMonthEstimateCost != 0 &&
-		clusterCostsSummary.ClusterAvgDailyCost != 0 &&
-		clusterCostsSummary.ClusterAvgHourlyCoreCost != 0 {
-		return true
+func ValidateSpecificClusterCostsSummary(clusterSummary *api.ClusterCostsSummary) bool {
+	if clusterSummary.ClusterConnectionSate != "running" ||
+		clusterSummary.ClusterMonthCostCurrent == 0 ||
+		clusterSummary.ClusterMonthEstimateCost == 0 ||
+		clusterSummary.ClusterAvgDailyCost == 0 ||
+		clusterSummary.ClusterAvgHourlyCoreCost == 0 {
+		return false
 	}
-
-	return false
-}
-
-func ValidateSpecificClusterComputeCosts(computeCosts *api.ClusterResourceCostList) bool {
-	// if computeCosts.ClusterId != "" &&
-	// 	len(computeCosts.Items) != 0 &&
-	// 	computeCosts.Items[0].CostOnDemandBillingMode != 0 &&
-	// 	computeCosts.Items[0].CPUCostOnDemandBillingMode != 0 &&
-	// 	computeCosts.Items[0].CPUCoreCountOnDemandBillingMode != 0 &&
-	// 	computeCosts.Items[0].RAMCostOnDemandBillingMode != 0 &&
-	// 	computeCosts.Items[0].RAMGBCountOnDemandBillingMode != 0 {
-	// 	return true
-	// }
-
-	return false
-}
-
-func ValidateSpecificClusterWorkloadCosts(workloadCosts *api.ClusterWorkloadCostList) bool {
-	// if workloadCosts.ClusterId == "" ||
-	// 	len(workloadCosts.Items) == 0 {
-	// 	return false
-	// }
-
-	// for _, ele := range workloadCosts.Items {
-	// 	// In kind cluster, these two workloads don't have resource requirements by default
-	// 	if strings.Contains(ele.WorkloadName, "kube-proxy") ||
-	// 		strings.Contains(ele.WorkloadName, "local-path") {
-	// 		continue
-	// 	}
-	// 	if ele.CostList[0].CostOnDemandBillingMode == 0 {
-	// 		return false
-	// 	}
-	// }
 
 	return true
 }
